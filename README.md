e5d75620314eefac28201767939881cc7651ac4f
Chris Kempson <git@chriskempson.com>
2012-08-30 20:54:34 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-base16-shell
-============
+# Base16 Shell
+Changes your shell's first 21 colors via escape sequences.
+Make sure this script is each time you open a new shell!
 
-Base16 for Shells
+![Base16 Shell](https://raw.github.com/chriskempson/base16-shell/master/base16-shell.png)
---
+++ base16.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+# Base 16 - Console color setup script
+# Chris Kempson (http://chriskempson.com)
+
+color00="151515" # Base 00 - Black
+color01="ac4142" # Base 08 - Red
+color02="90a959" # Base 0B - Green
+color03="f4bf75" # Base 0A - Yellow
+color04="6a9fb5" # Base 0D - Blue
+color05="aa759f" # Base 0E - Magenta
+color06="75b5aa" # Base 0C - Cyan
+color07="d0d0d0" # Base 05 - White
+color08="505050" # Base 03 - Bright Black
+color09=$color01 # Base 08 - Bright Red
+color10=$color02 # Base 0B - Bright Green
+color11=$color03 # Base 0A - Bright Yellow
+color12=$color04 # Base 0D - Bright Blue
+color13=$color05 # Base 0E - Bright Magenta
+color14=$color06 # Base 0C - Bright Cyan
+color15="f5f5f5" # Base 07 - Bright White
+color16="d2/84/45" # Base 09
+color17="8f/55/36" # Base 0F
+color18="20/20/20" # Base 01
+color19="30/30/30" # Base 02
+color20="b0/b0/b0" # Base 04
+color21="e0/e0/e0" # Base 06
+
+# Bright colors that are actually bright!
+# color09="ffa4ac" # Base XX - Bright Red
+# color10="c6e9a5" # Base XX - Bright Green
+# color11="ffdabf" # Base XX - Bright Yellow
+# color12="b1ecf7" # Base XX - Bright Blue
+# color13="ffd4f4" # Base XX - Bright Magenta
+# color14="bbffef" # Base XX - Bright Cyan
+
+# 16 color space
+printf "\e]P0$color00\e\\"
+printf "\e]P1$color01\e\\"
+printf "\e]P2$color02\e\\"
+printf "\e]P3$color03\e\\"
+printf "\e]P4$color04\e\\"
+printf "\e]P5$color05\e\\"
+printf "\e]P6$color06\e\\"
+printf "\e]P7$color07\e\\"
+printf "\e]P8$color08\e\\"
+printf "\e]P9$color09\e\\"
+printf "\e]PA$color10\e\\"
+printf "\e]PB$color11\e\\"
+printf "\e]PC$color12\e\\"
+printf "\e]PD$color13\e\\"
+printf "\e]PE$color14\e\\"
+printf "\e]PF$color15\e\\"
+
+# 256 color space
+printf "\e]4;16;rgb:$color16\e\\"
+printf "\e]4;17;rgb:$color17\e\\"
+printf "\e]4;18;rgb:$color18\e\\"
+printf "\e]4;19;rgb:$color19\e\\"
+printf "\e]4;20;rgb:$color20\e\\"
+printf "\e]4;21;rgb:$color21\e\\"
