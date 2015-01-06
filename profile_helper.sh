e8b7cbfd5002f99878589cce5e4e442f4b344517
JohnMorales <john.morales@exelisinc.com>
2015-01-05 19:35:54 -0500

    Adding profile helper script to quickly switch and persist changes

---
+++ profile_helper.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+script_dir=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
+for script in $script_dir/base16*.sh; do
+  script_name=$(basename $script .sh)
+  theme=${script_name#*-}
+  variation=${theme#*.}
+  theme=${theme%.*}
+  cat <<FUNC
+base16_${theme}_${variation}()
+{
+  . $script
+  ln -fs $script ~/.base16_theme 
+  export BASE16_THEME=base16-${theme}
+  export BASE16_VARIATION=$variation
+  cat <<vimrc_background > ~/.vimrc_background
+set background=$variation
+colorscheme base16-$theme
+vimrc_background
+}
+FUNC
+done;
+[ -f ~/.base16_theme ] && echo ". ~/.base16_theme"
