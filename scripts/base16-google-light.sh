3c8177f7dafc67021726b2d9bb6f0d8d34083f7d
Chris Kempson <git@chriskempson.com>
2016-06-29 12:04:14 +0100

    Adds Google Light scheme

--- scripts/base16-google-light.sh
+++ scripts/base16-google-light.sh
@@ -1,38 +1,38 @@
 #!/bin/sh
 # base16-shell (https://github.com/chriskempson/base16-shell)
 # Base16 Shell template by Chris Kempson (http://chriskempson.com)
-# Google scheme by Seth Wright (http://sethawright.com)
+# Google Light scheme by Seth Wright (http://sethawright.com)
 
 # This script doesn't support linux console (use 'vconsole' template instead)
 if [ "${TERM%%-*}" = 'linux' ]; then
     return 2>/dev/null || exit 0
 fi
 
-color00="1d/1f/21" # Base 00 - Black
+color00="ff/ff/ff" # Base 00 - Black
 color01="CC/34/2B" # Base 08 - Red
 color02="19/88/44" # Base 0B - Green
 color03="FB/A9/22" # Base 0A - Yellow
 color04="39/71/ED" # Base 0D - Blue
 color05="A3/6A/C7" # Base 0E - Magenta
 color06="39/71/ED" # Base 0C - Cyan
-color07="c5/c8/c6" # Base 05 - White
-color08="96/98/96" # Base 03 - Bright Black
+color07="37/3b/41" # Base 05 - White
+color08="b4/b7/b4" # Base 03 - Bright Black
 color09=$color01 # Base 08 - Bright Red
 color10=$color02 # Base 0B - Bright Green
 color11=$color03 # Base 0A - Bright Yellow
 color12=$color04 # Base 0D - Bright Blue
 color13=$color05 # Base 0E - Bright Magenta
 color14=$color06 # Base 0C - Bright Cyan
-color15="ff/ff/ff" # Base 07 - Bright White
+color15="1d/1f/21" # Base 07 - Bright White
 color16="F9/6A/38" # Base 09
 color17="39/71/ED" # Base 0F
-color18="28/2a/2e" # Base 01
-color19="37/3b/41" # Base 02
-color20="b4/b7/b4" # Base 04
-color21="e0/e0/e0" # Base 06
-color_foreground="c5/c8/c6" # Base 05
-color_background="1d/1f/21" # Base 00
-color_cursor="c5/c8/c6" # Base 05
+color18="e0/e0/e0" # Base 01
+color19="c5/c8/c6" # Base 02
+color20="96/98/96" # Base 04
+color21="28/2a/2e" # Base 06
+color_foreground="37/3b/41" # Base 05
+color_background="ff/ff/ff" # Base 00
+color_cursor="37/3b/41" # Base 05
 
 if [ -n "$TMUX" ]; then
   # Tell tmux to pass the escape sequences through
@@ -80,13 +80,13 @@ printf $printf_template 21 $color21
 # foreground / background / cursor color
 if [ -n "$ITERM_SESSION_ID" ]; then
   # iTerm2 proprietary escape codes
-  printf $printf_template_custom Pg c5c8c6 # forground
-  printf $printf_template_custom Ph 1d1f21 # background
-  printf $printf_template_custom Pi c5c8c6 # bold color
-  printf $printf_template_custom Pj 373b41 # selection color
-  printf $printf_template_custom Pk c5c8c6 # selected text color
-  printf $printf_template_custom Pl c5c8c6 # cursor
-  printf $printf_template_custom Pm 1d1f21 # cursor text
+  printf $printf_template_custom Pg 373b41 # forground
+  printf $printf_template_custom Ph ffffff # background
+  printf $printf_template_custom Pi 373b41 # bold color
+  printf $printf_template_custom Pj c5c8c6 # selection color
+  printf $printf_template_custom Pk 373b41 # selected text color
+  printf $printf_template_custom Pl 373b41 # cursor
+  printf $printf_template_custom Pm ffffff # cursor text
 else
   printf $printf_template_var 10 $color_foreground
   printf $printf_template_var 11 $color_background
