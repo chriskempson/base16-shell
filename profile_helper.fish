43d1fa95d8130e41ffa7f3b91941e6ceb9100b74
Jun Zhou <junz@junz.info>
2017-08-25 14:27:11 -0700

    [fish] strip "base16-" from env var BASE16_THEME

--- profile_helper.fish
+++ profile_helper.fish
@@ -26,7 +26,7 @@ for SCRIPT in $SCRIPT_DIR/scripts/*.sh
   function $THEME -V SCRIPT -V THEME
     eval sh $SCRIPT
     ln -sf $SCRIPT ~/.base16_theme
-    set -x BASE16_THEME $THEME
+    set -x BASE16_THEME (string split -m 1 '-' $THEME)[2]
     echo -e "if !exists('g:colors_name') || g:colors_name != '$THEME'\n  colorscheme $THEME\nendif" >  ~/.vimrc_background
   end
 end for
