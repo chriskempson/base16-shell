71e61bcd7910d9c69277c8d4279dd293452f43fb
Fábio Antunes <FabioAntunes@users.noreply.github.com>
2018-11-11 01:08:18 +0000

    Set vim background based on the theme
    If the theme has `light` on the name or if it is one of the hardcoded ones we should `set background=light`

--- profile_helper.fish
+++ profile_helper.fish
@@ -22,10 +22,16 @@ end
 # set aliases, like base16_*...
 for SCRIPT in $SCRIPT_DIR/scripts/*.sh
   set THEME (basename $SCRIPT .sh)
+  set LIGHT_THEMES  "base16-cupcake" "base16-brushtrees" "base16-cupertino" "base16-github" "base16-shapeshifter" "base16-tomorrow"
   function $THEME -V SCRIPT -V THEME
     eval sh '"'$SCRIPT'"'
     ln -sf $SCRIPT ~/.base16_theme
     set -x BASE16_THEME (string split -m 1 '-' $THEME)[2]
-    echo -e "if !exists('g:colors_name') || g:colors_name != '$THEME'\n  colorscheme $THEME\nendif" >  ~/.vimrc_background
+    if string match -q -- '*light*' $THEME; or contains $THEME $LIGHT_THEMES
+      set BACKGROUND 'light'
+    else
+      set BACKGROUND 'dark'
+    end
+    echo -e "if !exists('g:colors_name') || g:colors_name != '$THEME'\n  colorscheme $THEME\n  set background=$BACKGROUND\nendif" >  ~/.vimrc_background
   end
 end for
